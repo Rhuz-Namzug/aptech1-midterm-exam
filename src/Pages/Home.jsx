bea109887e1e820e8cf3db14f4f1cc0d0465c23c
guzman_r <guzman_r@ca01.ub.lan>
2026-03-23 13:25:00 +0800

    Guide to install

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Home = () => <h1>Home Page</h1>;
+const Home = () => <h1><center>Home Page</center></h1>;
 
 export default Home;
--- src/Pages/Success.jsx
+++ src/Pages/Success.jsx
@@ -1,5 +1,5 @@
 import React from "react";
 
-const Success = () => <h1><center>Success!</center></h1>;
+const Success = () => <h1><center>Success! <br /> You may now continue to the website!</center></h1>;
 
 export default Success;
