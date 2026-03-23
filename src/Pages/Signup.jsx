ba4b8e51a2657d5b53bcb43e2a0b9b685461ea5d
guzman_r <guzman_r@ca01.ub.lan>
2026-03-23 11:59:38 +0800

    Implemented validation logic

--- src/Pages/Signup.jsx
+++ src/Pages/Signup.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router-dom";
 function Signup() {
   const navigate = useNavigate();
 
-  // Controlled Form State
   const [formData, setFormData] = useState({
     firstName: "",
     lastName: "",
@@ -13,7 +12,41 @@ function Signup() {
     email: ""
   });
 
-  // Handle input changes
+  const [errors, setErrors] = useState({});
+
+  const nameRegex = /^[A-Za-z]{2,}$/;
+  const usernameRegex = /^[A-Za-z0-9._-]+$/;
+  const passwordRegex =/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{8,16}$/;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+  const validate = () => {
+    let newErrors = {};
+
+    if (!nameRegex.test(formData.firstName)) {
+      newErrors.firstName = "First name must be letters only and at least 2 characters.";
+    }
+
+    if (!nameRegex.test(formData.lastName)) {
+      newErrors.lastName = "Last name must be letters only and at least 2 characters.";
+    }
+
+    if (!usernameRegex.test(formData.username)) {
+      newErrors.username = "Username must contain only letters, numbers, ., _, -";
+    }
+
+    if (!passwordRegex.test(formData.password)) {
+      newErrors.password = "Password must be 8-16 characters with AT LEAST 1 uppercase, 1 lowercase, 1 number, and 1 special character.";
+    }
+
+    if (!emailRegex.test(formData.email)) {
+      newErrors.email = "Invalid email format.";
+    }
+
+    setErrors(newErrors);
+
+    return Object.keys(newErrors).length === 0;
+  };
+
   const handleChange = (e) => {
     const { name, value } = e.target;
 
@@ -23,13 +56,15 @@ function Signup() {
     });
   };
 
-  // Handle submit
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    console.log(formData);
-
-    navigate("/success");
+    if (validate()) {
+      console.log("Form is valid:", formData);
+      navigate("/success");
+    } else {
+      console.log("Validation errors");
+    }
   };
 
   return (
@@ -43,7 +78,8 @@ function Signup() {
           placeholder="First Name"
           value={formData.firstName}
           onChange={handleChange}
-        /><br />
+        />
+        <p>{errors.firstName}</p>
 
         <input
           type="text"
@@ -51,7 +87,8 @@ function Signup() {
           placeholder="Last Name"
           value={formData.lastName}
           onChange={handleChange}
-        /><br />
+        />
+        <p>{errors.lastName}</p>
 
         <input
           type="text"
@@ -59,7 +96,8 @@ function Signup() {
           placeholder="Username"
           value={formData.username}
           onChange={handleChange}
-        /><br />
+        />
+        <p>{errors.username}</p>
 
         <input
           type="password"
@@ -67,7 +105,8 @@ function Signup() {
           placeholder="Password"
           value={formData.password}
           onChange={handleChange}
-        /><br />
+        />
+        <p>{errors.password}</p>
 
         <input
           type="email"
@@ -75,7 +114,8 @@ function Signup() {
           placeholder="Email"
           value={formData.email}
           onChange={handleChange}
-        /><br />
+        />
+        <p>{errors.email}</p>
 
         <button type="submit">Submit</button>
       </form>
