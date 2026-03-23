ead56c9a7bdab9a6f5d48eeedf8f61eec7a9b02a
guzman_r <guzman_r@ca01.ub.lan>
2026-03-23 11:15:31 +0800

    Created navigation to implement routing

---
+++ src/Navigation/NavBar.jsx
@@ -0,0 +1,21 @@
+import { Link } from "react-router-dom";
+import "./NavBar.css";
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+
+      <h2 className="logo">Student Registration</h2>
+
+      <div className="nav-links">
+        <Link to="/Home">Home</Link>
+        <Link to="/Signup">Signup</Link>
+        <Link to="/Success">Success</Link>
+        <Link to="/Profile">Profile</Link>
+      </div>
+
+    </nav>
+  );
+}
+
+export default Navbar;
---
+++ src/Pages/Success.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Success = () => <h1>Success Page</h1>;
+
+export default Success;
